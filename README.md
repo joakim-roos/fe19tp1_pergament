e7814bbe72f0009f68dfd9bc268905072ad628ac
malin DH <m.hjerpe@live.se>
2019-10-28 14:38:10 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fe19tp1_teampergament
 FE19 Team Project 1. 
+
+https://trello.com/invite/teampergament/bc26d0f282b889006d70bf850c1f27c5
--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+console.log("test")
 
+document.getElementById("showHideBtn").addEventListener("click", showHideFunction);
+
+function showHideFunction(){
+    if()
+    document.getElementById("innerSideBar").style.display = "block";
+    console.log("hejsan");
+}
