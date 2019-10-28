809e03075a8e174eab23b7b144e611dc7de427bd
Joakim Roos <joakim.roos@student.kyh.se>
2019-10-28 22:46:32 +0100

    Added text editor and some styling.

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-console.log("test")
 
+//Fabian
 document.getElementById("showHideBtn").addEventListener("click", showHideFunction);
 
 function showHideFunction(){
-    if()
+    /* if() */
     document.getElementById("innerSideBar").style.display = "block";
-    console.log("hejsan");
+    //console.log("hejsan");
 }
+
+
+// Quill.js text editor. Not used at the moment. 
+let quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
+  
+
+
+  // TINY MCE Rich text editor. 
+  tinymce.init({
+    selector:'textarea',
+    menubar: false,
+    fixed_toolbar_container: '.form',
+    plugins: "autoresize",
+    min_height: 600,
+    max_height: 600,
+    content_css: '/style.css',
+    body_class: 'editorStyling'
+});
