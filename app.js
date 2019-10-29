15f77cf4b286ac84283accd261c3f2ce6bd43303
fabbethoma <55735538+fabbethoma@users.noreply.github.com>
2019-10-29 10:55:36 +0100

    Knappfix- funkar ej

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-
+var showBtn = document.getElementById("showHideBtn");
 //Fabian
-document.getElementById("showHideBtn").addEventListener("click", showHideFunction);
+showBtn.onclick = function() {showHideFunction()};
 
 function showHideFunction(){
-    /* if() */
-    document.getElementById("innerSideBar").style.display = "block";
+     if(document.getElementById("innerSideBar").style.display = "block"){
+      document.getElementById("innerSideBar").style.display = "none";
+      console.log(showBtn.style);
+     } 
+     else if(document.getElementById("innerSideBar").style.display = "none"){
+      document.getElementById("innerSideBar").style.display = "block";
+      console.log("2");
+     } else{
+      document.getElementById("innerSideBar").style.display = "block"
+     }
+    
     //console.log("hejsan");
 }
 
@@ -35,3 +44,37 @@ let quill = new Quill('#editor-container', {
     content_css: '/style.css',
     body_class: 'editorStyling'
 });
+
+
+// Hjälp knapp 
+window.onload = function(){ 
+// Get the modal hela första diven 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var starta = document.getElementById("starta");
+
+var musöver = document.querySelector("p")
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+};
