bc0e6169053109589665529dec44643144c4f143
Joakim Roos <joakim.roos@student.kyh.se>
2019-10-31 11:45:38 +0100

    Added Quill.js instead of Tiny MCE

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ addNewNote.onclick = function () {
 }
 
 function addElement () { 
+  let editorContent = document.querySelector(".ql-editor").textContent;
+  console.log(editorContent);
   let currentNote = document.getElementById("innerSideBar"); 
   let child = currentNote.firstChild;
   let newNote = document.createElement("div"); 
   newNote.className = "newNote";
-  newNote.innerHTML = tinyMCE.get('printableArea').getContent();
+  newNote.innerHTML = editorContent;  
+  /* newNote.innerHTML = tinyMCE.get('printableArea').getContent(); */
   currentNote.insertBefore(newNote, child);
 };
+
+
+  
 
 
 
@@ -34,7 +40,7 @@ function showHideFunction(){
 
 
   // TINY MCE config. 
-  tinymce.init({
+/*   tinymce.init({
     selector:'textarea',
     menubar: false,
     fixed_toolbar_container: '.form',
@@ -44,34 +50,36 @@ function showHideFunction(){
     content_css: '/style.css, https://fonts.googleapis.com/css?family=Raleway&display=swap',
     body_class: 'editorStyling',
     editor_selector: "editor",
+  }); */
+
+  let quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Team Pergament är cool',
+    theme: 'snow'  // or 'bubble'
   });
 
 
 
 // Hjälp knapp - Malin
+var modal = document.getElementById("myModal"); // Get the modal hela första diven 
+var btn = document.getElementById("helpBtn"); // Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 
-// Get the modal hela första diven 
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("helpBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function() {  // When the user clicks the button, open the modal 
   modal.style.display = "block";
-  // console.log("hello");
 }
-modal
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+
+span.onclick = function() { // When the user clicks on <span> (x), close the modal
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function(event) { // When the user clicks anywhere outside of the modal, close it
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -80,13 +88,10 @@ window.onclick = function(event) {
 
 // Print-knapp - Malin 
 printDiv = function(divName) {
-  var printContents = document.getElementById(divName).innerHTML;
+  var printContents = document.querySelector(".ql-editor").innerHTML;
   var originalContents = document.body.innerHTML;
-
   document.body.innerHTML = printContents;
-
   window.print();
-
 //  document.body.innerHTML = originalContents;
 location.reload();
 }
