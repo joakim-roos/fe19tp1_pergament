9eb3d2c70e4f7d626b97a078336f82eef48ffb51
Joakim Roos <joakim.roos@student.kyh.se>
2019-11-07 10:33:13 +0100

    changes

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-// let how =  document.getElementById("how").textContent;
-// window.alert(how);
 
-// Joakim - 'Skapa en anteckning' - Submit-knappen funkar och lägger till en anteckning på innersidebar. Dock går det inte att klicka på den än.
+
+  let quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block'],
+        [{ 'list': 'ordered'}, {'list': 'bullet' }],
+      ]
+    },
+    placeholder: 'Team Pergament är cool',
+    theme: 'snow'  // or 'bubble'
+  });
+
+
 const addNewNote = document.getElementById("newNoteBtn");
 const noteList=[]; //tom array för samtliga notes
 
 
+// testfunktion, används ej just nu
+let notes = [{
+  id: Date.now(),
+  data: "", //{(quill delta)}, // "<html>",
+  favourite: false,
+  deleted: false
+}];
 
-//var tänkt att få lägsta öppna id, rätt dålig funktion men fick inte Lambda att fungera i JS.
-//istället returnerar funktionen n efter n calls
-/* function getOpenId(){
-  Count++;
-  return Count;
-  console.log(Count);
-} */
+let quillDelta = notes[0].data
+ //
+
 
 addNewNote.onclick = function () {
   addElement();
@@ -29,32 +44,24 @@ function addElement () {
   currentNote.insertBefore(newNote, child);
 
   // Skapar ett ID och spar i variabel 
-  let datumet = Date.now();
-  let skapatID = Math.floor(datumet / 1000);
-  newNote.setAttribute('id', skapatID); //ger div(newnote) det skapade ID:t 
+  let date = Date.now();
+  let noteNumberX = Math.floor(date / 1000);
+  newNote.setAttribute('id', noteNumberX); //ger div(newnote) det skapade ID:t 
 
-
-                //ger div-noten ett nytt, "mer säkert", id
-                //let newId=getOpenId();
-                //newNote.setAttribute("testId",newId);
-                //lägger till den i globala listan
-  newNote.setAttribute("onclick", "focusElement("+skapatID+")"); //byter valt element vid klick
+  newNote.setAttribute("onclick", "focusElement("+noteNumberX+")"); //byter valt element vid klick
   noteList.push(newNote);
-// det här ovanför ska tas bort. 
 
-
-  let raderaKnapp = document.createElement("button");   //skapa raderaknapp
-  raderaKnapp.className="ta-bort-knappen"; // ger knapp en klass för att kunna styla det 
-  raderaKnapp.setAttribute("onclick", "removeElement("+skapatID+")"); //ger knapp specifikt ID och kör funktion vid klick 
-  let textRaderaKnapp = document.createTextNode("X");  //ger radera knapp ett innehåll 
-  raderaKnapp.appendChild(textRaderaKnapp);   //slår ihop knappen med innehållet 
-  newNote.appendChild(raderaKnapp);   //lägger knappen i div (newnote) 
+  let deleteButton = document.createElement("button");   //skapa raderaknapp
+  deleteButton.className="delete-button"; // ger knapp en klass för att kunna styla det 
+  deleteButton.setAttribute("onclick", "removeElement("+noteNumberX+")"); //ger knapp specifikt ID och kör funktion vid klick 
+  let textdeleteButton = document.createTextNode("X");  //ger radera knapp ett innehåll 
+  deleteButton.appendChild(textdeleteButton);   //slår ihop knappen med innehållet 
+  newNote.appendChild(deleteButton);   //lägger knappen i div (newnote) 
 };
 
 
- 
-function focusElement(skapatID){  //när man klickar på en note kallas den här funktionen. 
-  var selectedNote = Id2Note(skapatID);
+function focusElement(noteNumberX){  //när man klickar på en note kallas den här funktionen. 
+  var selectedNote = Id2Note(noteNumberX);
   quill.root.innerHTML = selectedNote.innerHTML;
   }
 
@@ -66,24 +73,26 @@ function Id2Note(int){ // Loopar igenom notelist array genom Malins ID
     t = noteList[i].getAttribute("id");
       if(int==t){
       return noteList[i]
+      
       }
   }
+  console.log(noteList[i]);
 }
 
 
 
 //Malin
 //klickfunktionen
-function removeElement(skapatID) {
+function removeElement(noteNumberX) {
   
-  let attRadera = document.getElementById(skapatID); // spar anteckningen i en varabel 
-  attRadera.parentNode.removeChild(attRadera); //raderar anteckningen
+  let deleteItem = document.getElementById(noteNumberX); // spar anteckningen i en varabel 
+  deleteItem.parentNode.removeChild(deleteItem); //raderar anteckningen
 
 
   var i;    //går igenom globala listan och tar bort noten ur listan
   var index;
   for (i = 0; i < noteList.length; i++){
-    if (noteList[i].skapatID === skapatID){
+    if (noteList[i].noteNumberX === noteNumberX){
       index=i;
     }
   }
@@ -107,40 +116,22 @@ function showHideFunction(){
      }
 }
 
-  let quill = new Quill('#editor-container', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['image', 'code-block'],
-        [{ 'list': 'ordered'}, {'list': 'bullet' }],
-      ]
-    },
-    placeholder: 'Team Pergament är cool',
-    theme: 'snow'  // or 'bubble'
-  });
-
-
 
 // Hjälp knapp - Malin
-let modal = document.getElementById("myModal"); // Get the modal hela första diven 
+let modal = document.getElementById("myModal"); // första diven 
 let btn = document.getElementById("helpBtn"); // Get the button that opens the modal
 let span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
-
 btn.onclick = function() {  // When the user clicks the button, open the modal 
   modal.style.display = "block";
 }
-
 span.onclick = function() { // When the user clicks on <span> (x), close the modal
   modal.style.display = "none";
 }
-
 window.onclick = function(event) { // When the user clicks anywhere outside of the modal, close it
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
 
 // Print-knapp - Malin 
 printDiv = function(divName) {
